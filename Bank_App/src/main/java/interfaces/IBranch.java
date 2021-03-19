3db3db597277c017803bbeda4197342c13298eba
felixportuphy <77155009+felixportuphy@users.noreply.github.com>
2021-03-19 08:41:10 +0000

    modified each interface in package interfaces

---
+++ Bank_App/src/main/java/interfaces/IBranch.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import classes.Customer;
+
+import java.util.ArrayList;
+
+public interface IBranch {
+     String getName();
+     ArrayList<Customer> getCustomer();
+      boolean newCustomer(String customerName,double initialTransaction);
+     boolean addCustomerTransaction(String customerName,double transaction);
+
+}
